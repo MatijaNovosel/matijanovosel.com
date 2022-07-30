07e0881b24eab57bbe5025340f9ce58b59ec0be6
Matija Novosel <mnovosel5@gmail.com>
2022-07-30 13:27:03 +0200

    Tweaks

--- src/pages/BlogIndex.vue
+++ src/pages/BlogIndex.vue
@@ -1,20 +1,11 @@
 <template>
   <q-page class="q-pt-none q-pb-lg q-pt-md-lg">
-    <q-btn
-      v-show="$route.name !== RouteNames.BLOGS"
-      size="sm"
-      no-caps
-      color="transparent"
-      class="q-mb-lg rounded-btn q-pl-none q-mt-md q-mt-md-none"
-      :to="{ name: RouteNames.BLOGS }"
-    >
-      <q-icon right name="mdi-arrow-left" class="q-mr-sm" />
-      <div class="text-h6">Back</div>
-    </q-btn>
+    <BlogBackButton v-if="$route.name !== RouteNames.BLOGS" />
     <router-view />
   </q-page>
 </template>
 
 <script lang="ts" setup>
+import BlogBackButton from "src/components/BlogBackButton.vue";
 import { RouteNames } from "src/router/routeNames";
 </script>
--- src/pages/blogs/1ScriptSetupVsDefineComponent.vue
+++ src/pages/blogs/1ScriptSetupVsDefineComponent.vue
@@ -1,29 +1,31 @@
 <template>
-  <div
-    class="blog-img q-mb-md"
-    :style="{
-      backgroundImage: 'url(/vue.jpg)',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center center',
-      height: `${smAndDown() ? 350 : 400}px`
-    }"
-  />
-  <div class="column q-my-lg">
-    <span :class="smAndDown() ? 'text-h4' : 'text-h3'" class="text-bold">
-      &lt;script setup&gt; vs defineComponent
-    </span>
-    <span class="text-grey-5 q-mt-md">
-      Published on
-      <span class="text-orange-4">
-        {{ formatMonth(new Date()) }}
+  <div>
+    <div
+      class="blog-img q-mb-md"
+      :style="{
+        backgroundImage: 'url(/vue.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        height: `${smAndDown() ? 350 : 400}px`
+      }"
+    />
+    <div class="column q-my-lg">
+      <span :class="smAndDown() ? 'text-h4' : 'text-h3'" class="text-bold">
+        &lt;script setup&gt; vs defineComponent
       </span>
-    </span>
-  </div>
-  <div class="bg-dark q-pa-md q-mb-xl blog-desc">
-    <span>
-      With the creation of Vue 3 the community has been introduced to a new, more function oriented, way of
-      coding.
-    </span>
+      <span class="text-grey-5 q-mt-md">
+        Published on
+        <span class="text-orange-4">
+          {{ formatMonth(new Date()) }}
+        </span>
+      </span>
+    </div>
+    <div class="bg-dark q-pa-md q-mb-xl blog-desc">
+      <span>
+        With the creation of Vue 3 the community has been introduced to a new, more function oriented, way of
+        coding.
+      </span>
+    </div>
   </div>
 </template>
 
