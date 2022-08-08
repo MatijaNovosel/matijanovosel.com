7021f5d2c9d5d6fbb18860105e3047e5bcb34441
Matija Novosel <mnovosel5@gmail.com>
2022-08-08 14:46:09 +0200

    Blog entries wip

---
+++ components/alert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="rounded-lg p-5 text-left flex items-center bg-dark-400">
+    <div class="mr-5 text-3xl">🛑</div>
+    <div>
+      <div class="text-lg text-red-400">
+        {{ title }}
+      </div>
+      <div class="text-sm mt-1">
+        {{ subtitle }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: ""
+  },
+  subtitle: {
+    type: String,
+    required: true,
+    default: ""
+  }
+});
+</script>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,13 +2,25 @@
   <div class="mt-10 w-full">
     <NuxtLink to="/blog">
       <button
-        class="ripple project-btn px-2 py-1 font-medium text-xl text-gray-100 rounded flex items-center justify-center"
+        class="ripple project-btn pr-3 pl-2 py-1 font-medium text-xl text-gray-100 rounded flex items-center justify-center"
       >
         <IconArrowLeft class="mr-2" />
         <h6>Back</h6>
       </button>
     </NuxtLink>
-    <div class="bg-dark-200 mt-5 rounded-lg p-5">
+    <h1
+      class="font-bold text-5xl md:text-7xl text-center font-space"
+      v-if="loading"
+    >
+      <Spinner />
+    </h1>
+    <alert
+      title="Something went wrong"
+      subtitle="An error ocurred while loading the blog entry"
+      class="mt-5"
+      v-else-if="error"
+    />
+    <div class="bg-dark-200 mt-5 rounded-lg p-5" v-else>
       <h1>Neka ruta: {{ $route.fullPath }}</h1>
     </div>
   </div>
@@ -18,11 +30,18 @@
 import IconArrowLeft from "~icons/mdi/arrow-left";
 
 const route = useRoute();
+const loading = ref(true);
+const error = ref(true);
 
-const { data, pending, error } = await useLazyAsyncData(
-  `blog-${route.params.slug}`,
-  () => $fetch(`/api/blog/${route.params.slug}`)
-);
+onMounted(async () => {
+  const { data, pending, error } = await useLazyAsyncData(
+    `blog-${route.params.slug}`,
+    () => $fetch(`/api/blog/${route.params.slug}`)
+  );
+  setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+});
 
 const { setMeta } = useMetadata();
 setMeta("Matija Novosel - Blogs");
