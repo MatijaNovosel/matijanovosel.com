237b45aeba358074d4b6f4bf784df1d980e4d7b4
Matija Novosel <mnovosel5@gmail.com>
2022-08-12 23:14:33 +0200

    Tweak

--- components/projectCard.vue
+++ components/projectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-left w-full grid grid-cols-12 project-card bg-dark-800 mt-5">
     <div
-      class="project-img col-span-12 md:col-span-7 rounded-t-lg md:rounded-l-lg md:rounded-t-none"
+      class="project-img col-span-12 md:col-span-7 rounded-tr-lg rounded-tl-lg md:rounded-tr-none md:rounded-bl-lg"
       :style="{
         backgroundImage: `url(${project.img})`
       }"
