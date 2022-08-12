77f6004df85c4d7c1b19bb9e07553a585915fa36
Matija Novosel <mnovosel5@gmail.com>
2022-08-13 01:22:51 +0200

    Project links

--- components/projectCard.vue
+++ components/projectCard.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="text-left w-full grid grid-cols-12 project-card bg-dark-800 mt-5">
+  <NuxtLink :href="project.link">
     <div
-      class="project-img col-span-12 md:col-span-7 rounded-tr-lg rounded-tl-lg md:rounded-tr-none md:rounded-bl-lg"
-      :style="{
-        backgroundImage: `url(${project.img})`
-      }"
-    />
-    <div
-      class="col-span-12 md:col-span-5 px-4 flex flex-col justify-center my-5 md:my-0"
+      class="text-left w-full grid grid-cols-12 project-card bg-dark-800 mt-5 shrink"
     >
-      <span class="text-md font-bold text-left"> {{ project.title }} </span>
-      <span class="mt-2 text-gray-400 text-sm md:text-xs">
-        {{ project.description }}
-      </span>
+      <div
+        class="project-img col-span-12 md:col-span-7 rounded-tr-lg rounded-tl-lg md:rounded-tr-none md:rounded-bl-lg"
+        :style="{
+          backgroundImage: `url(${project.img})`
+        }"
+      />
+      <div
+        class="col-span-12 md:col-span-5 px-4 flex flex-col justify-center my-5 md:my-0"
+      >
+        <span class="text-md font-bold text-left"> {{ project.title }} </span>
+        <span class="mt-2 text-gray-400 text-sm md:text-xs">
+          {{ project.description }}
+        </span>
+      </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script lang="ts" setup>
 import { PropType } from "vue";
 import { ProjectItem } from "@/models";
-import IconOpenInNew from "~icons/material-symbols/open-in-new";
 
 defineProps({
   project: {
