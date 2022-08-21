4285c8b08f4a64c47ab0f2e8354fb1e4a7b46fe1
Matija Novosel <mnovosel5@gmail.com>
2022-08-21 22:29:23 +0200

    Fix

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="w-full flex justify-between z-2 md:pb-5 pt-7 px-1 md:px-0">
     <div>
-      <span class="text-sm md:text-base"> &copy; Matija Novosel 2020 </span>
+      <span class="text-sm md:text-base"> &copy; Matija Novosel 2022 </span>
     </div>
     <a
       href="https://github.com/MatijaNovosel/matijanovosel.com"
