8d38c8d3bef05d1000b69a90f335b56be4bd6341
Matija Novosel <mnovosel5@gmail.com>
2022-09-13 00:40:29 +0200

    LinkedIn URL

--- components/blog/contact.vue
+++ components/blog/contact.vue
@@ -8,10 +8,7 @@
         <a class="text-white" href="https://github.com/MatijaNovosel">
           <IconGithub :style="{ fontSize: '1.6em' }" />
         </a>
-        <a
-          class="ml-2"
-          href="https://www.linkedin.com/in/matija-novosel-616403161/"
-        >
+        <a class="ml-2" href="www.linkedin.com/in/matijanovosel">
           <IconLinkedin :style="{ fontSize: '1.6em' }" />
         </a>
       </div>
