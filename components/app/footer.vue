af496a14b8a51fc2213f09c94dbc29812c7753d4
Matija Novosel <mnovosel5@gmail.com>
2022-09-16 19:09:13 +0200

    About me page changed, footer changes

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -5,11 +5,26 @@
         &copy; Matija Novosel {{ new Date().getFullYear() }}
       </span>
     </div>
-    <a
-      href="https://github.com/MatijaNovosel/matijanovosel.com"
-      class="text-sm md:text-base text-gray-300 underline"
-    >
-      Source
-    </a>
+    <div>
+      <a class="text-white" href="https://github.com/MatijaNovosel">
+        <IconGithub :style="{ fontSize: '1.3em' }" />
+      </a>
+      <a class="text-white px-1" href="https://dev.to/matijanovosel">
+        <IconDevTo :style="{ fontSize: '1.3em' }" />
+      </a>
+      <a class="pr-1" href="mailto:mnovosel5@gmail.com">
+        <IconEmail :style="{ fontSize: '1.3em' }" />
+      </a>
+      <a href="https://www.linkedin.com/in/matijanovosel/">
+        <IconLinkedin :style="{ fontSize: '1.3em' }" />
+      </a>
+    </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+import IconLinkedin from "~icons/mdi/linkedin";
+import IconGithub from "~icons/mdi/github";
+import IconDevTo from "~icons/material-symbols/logo-dev";
+import IconEmail from "~icons/mdi/email";
+</script>
---
+++ components/blog/workExperience.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="font-bold pb-4 text-xl">Work experience</div>
+  <Timeline>
+    <TimelineItem time="August 2021 - Current">
+      <template #title>
+        <div class="flex flex-col">
+          <a
+            href="https://king-ict.com/"
+            class="text-white font-bold decoration-dotted underline underline-offset-5"
+          >
+            King ICT
+          </a>
+          <span class="mt-1 text-xs text-green-vue">
+            Software developer (student)
+          </span>
+        </div>
+      </template>
+      <div class="flex flex-col text-sm space-y-1 text-gray-300">
+        <div>• Software development using full stack web technologies</div>
+        <div>• Development of mobile applications</div>
+        <div>• Mentoring and creating educational content</div>
+      </div>
+    </TimelineItem>
+    <TimelineItem time="July 2019 - July 2021">
+      <template #title>
+        <div class="flex flex-col">
+          <a
+            href="https://www.omega-software.hr/"
+            class="text-white font-bold decoration-dotted underline underline-offset-5"
+          >
+            Omega software
+          </a>
+          <span class="mt-1 text-xs text-green-vue">
+            Junior software developer (student)
+          </span>
+        </div>
+      </template>
+      <div class="flex flex-col text-sm space-y-1 text-gray-300">
+        <div>
+          • Software development on multiple company projects using full stack
+          web technologies
+        </div>
+        <div>
+          • Mentoring and creating educational content for new and future
+          employees
+        </div>
+      </div>
+    </TimelineItem>
+    <TimelineItem time="May 2018 - February 2019">
+      <template #title>
+        <div class="flex flex-col">
+          <a
+            href="https://www.maidea.com/"
+            class="text-white font-bold decoration-dotted underline underline-offset-5"
+          >
+            Maidea
+          </a>
+          <span class="mt-1 text-xs text-green-vue">
+            Junior software developer (student)
+          </span>
+        </div>
+      </template>
+      <div class="flex flex-col text-sm space-y-1 text-gray-300">
+        <div>Software development using full stack web technologies.</div>
+      </div>
+    </TimelineItem>
+  </Timeline>
+</template>
