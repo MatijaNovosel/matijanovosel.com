4665dcddddf2f250d91a94d90114e2cde4bfcc36
Matija Novosel <mnovosel5@gmail.com>
2022-11-02 09:32:02 +0100

    Shortened css

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -1,25 +1,25 @@
 <template>
   <footer
-    class="w-full flex justify-between z-2 md:pb-5 pt-7 px-1 md:px-0 user-select-none"
+    class="w-full flex items-center justify-between z-2 md:pb-5 pt-7 px-1 md:px-0 user-select-none"
   >
     <div>
       <span class="text-sm md:text-base">
         &copy; Matija Novosel {{ new Date().getFullYear() }}
       </span>
     </div>
-    <div>
-      <a class="text-white" href="https://github.com/MatijaNovosel">
-        <IconGithub :style="{ fontSize: '1.3em' }" />
-      </a>
-      <a class="text-white px-1" href="https://dev.to/matijanovosel">
-        <IconDevTo :style="{ fontSize: '1.3em' }" />
-      </a>
-      <a class="pr-1" href="mailto:mnovosel5@gmail.com">
-        <IconEmail :style="{ fontSize: '1.3em' }" />
-      </a>
-      <a href="https://www.linkedin.com/in/matijanovosel/">
-        <IconLinkedin :style="{ fontSize: '1.3em' }" />
-      </a>
+    <div class="flex-center">
+      <NuxtLink to="https://github.com/MatijaNovosel">
+        <IconGithub />
+      </NuxtLink>
+      <NuxtLink class="text-white px-1" to="https://dev.to/matijanovosel">
+        <IconDevTo />
+      </NuxtLink>
+      <NuxtLink class="pr-1" to="mailto:mnovosel5@gmail.com">
+        <IconEmail />
+      </NuxtLink>
+      <NuxtLink to="https://www.linkedin.com/in/matijanovosel/">
+        <IconLinkedin />
+      </NuxtLink>
     </div>
   </footer>
 </template>
@@ -30,3 +30,10 @@ import IconGithub from "~icons/mdi/github";
 import IconDevTo from "~icons/material-symbols/logo-dev";
 import IconEmail from "~icons/mdi/email";
 </script>
+
+<style scoped>
+a {
+  @apply flex items-center;
+  font-size: 1.3em;
+}
+</style>
