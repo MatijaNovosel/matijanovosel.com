88d7b6790321efaa877cbf74f84cf94f8a2b55d0
Matija Novosel <mnovosel5@gmail.com>
2022-11-29 21:17:52 +0100

    Tweak

--- components/glitchText.vue
+++ components/glitchText.vue
@@ -34,9 +34,7 @@ defineProps({
   --clip-top: calc(var(--stack-height) * var(--index));
   --clip-bottom: calc(var(--stack-height) * var(--inverse-index));
   clip-path: inset(var(--clip-top) 0 var(--clip-bottom) 0);
-  animation: stack 340ms cubic-bezier(0.46, 0.29, 0, 1.24) 1 backwards
-      calc(var(--index) * 120ms),
-    glitch 2s ease infinite 2s alternate-reverse;
+  animation: glitch 2s ease infinite 2s alternate-reverse;
 }
 
 .stack span:nth-child(odd) {
@@ -44,26 +42,6 @@ defineProps({
 }
 .stack span:nth-child(even) {
   --glitch-translate: -8px;
-}
-
-@keyframes stack {
-  0% {
-    opacity: 0;
-    transform: translateX(-50%);
-    text-shadow: -2px 3px 0 red, 2px -3px 0 blue;
-  }
-  60% {
-    opacity: 0.5;
-    transform: translateX(50%);
-  }
-  80% {
-    transform: none;
-    opacity: 1;
-    text-shadow: 2px -3px 0 red, -2px 3px 0 blue;
-  }
-  100% {
-    text-shadow: none;
-  }
 }
 
 @keyframes glitch {
