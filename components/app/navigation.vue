689c53b86fc5c83af74642ce1f5a93e22225243b
Matija Novosel <mnovosel5@gmail.com>
2023-03-31 15:54:28 +0200

    Tweak

--- components/app/navigation.vue
+++ components/app/navigation.vue
@@ -1,14 +1,12 @@
 <template>
-  <nav
-    class="flex z-4 mt-5 bg-dark-400 md:bg-transparent fixed md:absolute text-white gap-2"
-  >
+  <nav class="flex z-4 mt-5 fixed md:absolute text-white gap-2">
     <nuxt-link
       :id="link.to"
       v-for="(link, i) in links"
       :key="i"
       active-class="active-tab"
       :to="link.to"
-      class="relative flex items-center text-xl md:text-3xl nav-tab border-1"
+      class="relative flex items-center text-xl md:text-3xl nav-tab border-1 bg-dark-400"
     >
       {{ link.text }}
     </nuxt-link>
